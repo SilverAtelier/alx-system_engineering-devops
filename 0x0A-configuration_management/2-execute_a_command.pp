8d695c4ab7598ad72398801f10dff167e5c24d0e
Al-Vaati01 <alvinvaati@gmail.com>
2023-01-12 18:50:20 +0300

    update-execute

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
 exec { 'killmenow':
-command => 'pkill -9 -f killmenow',
-path    => ['/usr/bin', '/usr/sbin', '/bin']
+command => 'pkill -f killmenow',
+path    => ['/usr/bin/', '/usr/sbin/', '/bin/']
 }
