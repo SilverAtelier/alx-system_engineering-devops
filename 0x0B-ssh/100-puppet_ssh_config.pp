58daa4c8afde6bfd30a39a508ed5279568ed6054
Al-Vaati01 <alvinvaati@gmail.com>
2023-01-13 22:43:47 +0300

    puppet confg

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,6 +4,7 @@ class ssh_config {
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
+    content	=> 'Host 54.82.117.241\nIdentityFile ~/.ssh/school\nPasswordAuthentication no',
     source => '~/.ssh/school',
   }
 }
