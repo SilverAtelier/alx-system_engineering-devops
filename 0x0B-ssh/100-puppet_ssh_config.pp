fd500c15100f81f735a114281e4555b83bf96f64
Al-Vaati01 <alvinvaati@gmail.com>
2023-01-13 22:57:39 +0300

    puppet confg

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,17 @@
-
-  file { '~/.ssh/school':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content  => 'Host 54.82.117.241\nIdentityFile ~/.ssh/school\nPasswordAuthentication no',
-    path    => '~/.ssh/school',
+class ssh_config {
+  file { '/etc/ssh/ssh_config':
+    ensure => present,
+    owner  => 'ubuntu',
+    group  => 'root',
+    mode   => '0644',
   }
+  file_line { 'Declare identity file':
+    path => '/etc/ssh/ssh_config',
+    line => 'IdentityFile ~/.ssh/school',
+  }
+  file_line { 'Turn off passwd auth':
+    path => '/etc/ssh/ssh_config',
+    line => 'PasswordAuthentication no',
+  }
+}
 
