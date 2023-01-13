0cc56611c8dfb4df2b0a70f20bd062f3c27a40de
Al-Vaati01 <alvinvaati@gmail.com>
2023-01-13 19:29:14 +0300

    execute cmd

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-exec { 'killmenow':
-command => 'pkill -f killmenow',
-path    => ['/usr/bin/', '/usr/sbin/', '/bin/']
+# Execute a command
+exec { 'kill-killmenow':
+  command => 'pkill killmenow',
+  path    => '/usr/bin';
 }
