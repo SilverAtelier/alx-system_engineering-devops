0db86d898d67885f1c67f76d33a991985bc3e5a1
Al-vaati01 <107029662+Al-vaati01@users.noreply.github.com>
2023-06-07 22:17:58 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,4 +3,6 @@ file { '/var/www/html/wp-settings.php':
   content => file('/var/www/html/wp-settings.php').content.gsub(/\b.phpp\b/, '.php'),
   require => Package['apache2'],
   notify  => Service['apache2'],
+  command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
+  provider => shell,
 }
