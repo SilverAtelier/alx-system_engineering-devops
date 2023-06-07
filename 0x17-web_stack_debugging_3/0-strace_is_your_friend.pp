e0194d5fc45c14dcad7f26119d481c93a35079ac
Al-vaati01 <107029662+Al-vaati01@users.noreply.github.com>
2023-06-07 22:14:57 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 #fixing wordpress
-exec { 'settingPress':
-  command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
-  provider => shell,
+file { '/var/www/html/wp-settings.php':
+  content => file('/var/www/html/wp-settings.php').content.gsub(/\b.phpp\b/, '.php'),
+  require => Package['apache2'],
+  notify  => Service['apache2'],
 }
