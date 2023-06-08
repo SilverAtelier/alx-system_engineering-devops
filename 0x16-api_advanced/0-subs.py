2dc0f50499634ea28f92dba26d69143838a1ee11
Alvin <alvinvaati@gmail.com>
2023-06-08 19:22:18 +0300

    unction that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Uses the reddit API to print the number of subscribers of a subreddit
+"""
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    
+    reddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'request'}
+    response = requests.get(reddit_url, headers=headers,
+                            allow_redirects=False)
+    if str(response) != "<Response [200]>":
+        return 0
+    r_json = response.json()
+    num_subs = r_json.get("data").get("subscribers")
+    return num_subs
